28eddf20b419f500c446128e8e779141186a3081
Walescko <walescko@gmail.com>
2022-09-09 23:08:44 -0300

    py - hello world and variables

---
+++ Basics/Variables.py
@@ -0,0 +1,19 @@
+x = 5
+print(x)
+print(type(x))
+print(x, type(x))
+
+preco = 19.99
+print(preco)
+print(type(preco))
+print(preco, type(preco))
+
+cidade = "São Paulo" #String - pode ser "" ou ''
+print(cidade)
+print(type(cidade))
+print(cidade, type(cidade))
+
+escolha = True #boolean
+print(escolha)
+print(type(escolha))
+print(escolha, type(escolha))
---
+++ Basics/helloWorld.py
@@ -0,0 +1,1 @@
+print("Hello World Santander! - Data Science")
