164606193a0299b1d96f9f2abe96121a82bf9695
Walescko <walescko@gmail.com>
2022-09-11 20:38:19 -0300

    py - operators

---
+++ Basics/operators.py
@@ -0,0 +1,34 @@
+#Operadores aritméticos
+a = 16
+b = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#operadores
+print(a**b)
+print(a//b) #divisao inteira
+print(a%b)
+
+#operadores booleanos
+haveBreads = True
+haveCoffe = False
+print(not haveBreads)
+print(haveCoffe or haveBreads)
+print(haveCoffe and haveBreads)
+
+#operadores relacionais
+dolar = 5.30
+real = 1.25
+print(real < dolar) #true
+print(real > dolar) #false
+print(real == dolar) #false
+print(real <= dolar) #true
+print(real >= dolar) #false
+print(real != dolar) #true
+
+
+
+
+
