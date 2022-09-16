826ade9a021d7cf46ee1ae309a6c32f2ef76f6c5
Walescko <walescko@gmail.com>
2022-09-15 23:14:26 -0300

    py - list

---
+++ AdvancesStructures/ListAndTuples.py
@@ -0,0 +1,14 @@
+countryNames = ['Brasil', 'Argentina', 'Estados Unidos', 'Canadá', 'Japão']
+print(countryNames)
+
+print('Tamanho da lista:', len(countryNames))
+
+print('País', countryNames[4])
+print('País', countryNames[-1])
+
+countryNames[4] = 'Polônia'
+
+print('Países', countryNames)
+print('País: ', countryNames[4])
+
+
