aedbccd731f3a30534a35ab5683f45ecf3bd3f36
Walescko <walescko@gmail.com>
2022-09-16 18:52:54 -0300

    py - lists

--- AdvancesStructures/ListAndTuples.py
+++ AdvancesStructures/ListAndTuples.py
@@ -10,5 +10,19 @@ countryNames[4] = 'Polônia'
 
 print('Países', countryNames)
 print('País: ', countryNames[4])
+# Slice
+print(countryNames[1:3])
+print(countryNames[1:-1])
+print(countryNames[1:])
+print(countryNames[:3])
+print(countryNames[:])
+print(countryNames[::2])
+print(countryNames[::-1])
+
+print('Brasil' in countryNames)
+print('Canada' not in countryNames)
+
+cityList = []
+
 
 
