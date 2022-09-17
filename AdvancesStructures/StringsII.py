277208031e28c15e0719ee72225ac26f4509c039
Walescko <walescko@gmail.com>
2022-09-17 20:01:59 -0300

    py - strings

---
+++ AdvancesStructures/StringsII.py
@@ -0,0 +1,12 @@
+greeting = "Hello, "
+name = "Walescko"
+print(greeting + name)
+print(name * 5)
+age = 42
+numbersSon = 0
+print(name + " tem " + str(age) + " anos e " + str(numbersSon) + ' filhos')
+print("{} tem {} anos e {} filhos.".format(name, age, numbersSon))
+
+priceGas = 4.889
+print("O preço da gasolina hoje subiu para R$ {:.2f}".format(priceGas))
+print(f'{name} tem {age} anos e {numbersSon} filhos.')
