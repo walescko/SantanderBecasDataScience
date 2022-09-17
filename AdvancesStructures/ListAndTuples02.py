592e50043d9708d05042315e2525a3d575d20e07
Walescko <walescko@gmail.com>
2022-09-17 07:35:16 -0300

    py - Tuples

---
+++ AdvancesStructures/ListAndTuples02.py
@@ -0,0 +1,15 @@
+#Tuplas não é possível editar os valores
+countryNames = ('Brasil', 'Polônia', 'Estados Unidos', 'Canadá')
+print(countryNames)
+print(type(countryNames))
+print(countryNames, type(countryNames))
+
+stateName = 'Rio Grande do Sul',
+print(stateName, type(countryNames))
+print(len(countryNames))
+print(stateName, len(stateName))
+print(countryNames[1])
+
+a, b, c, d = countryNames
+print(*countryNames)
+
