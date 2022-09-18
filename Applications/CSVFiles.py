117db596575a325b30e7a02390821453ea4ae06a
Walescko <walescko@gmail.com>
2022-09-18 18:21:11 -0300

    py CSV file write

--- Applications/CSVFiles.py
+++ Applications/CSVFiles.py
@@ -19,3 +19,13 @@ with open('brasil_covid.csv', 'r') as csvFile:
     for line in lines:
         line = line.split(',')
         print(line)
+
+with open('users.csv', 'w', newline='') as usersFile:
+    writer = csv.writer(usersFile)
+    writer.writerow(['nome', 'sobrenome', 'email', 'genero'])
+    writer.writerow(['Joey', 'Logano', 'logano@nascar.com', 'male'])
+
+with open('users.csv', 'r') as csvFile:
+    reader = csv.reader(csvFile)
+    for line in reader:
+        print(line)
