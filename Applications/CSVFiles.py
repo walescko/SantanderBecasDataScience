4c11acf606880848039cc08178f96e0775be6fac
Walescko <walescko@gmail.com>
2022-09-18 18:04:52 -0300

    py - open CSV and print

--- Applications/CSVFiles.py
+++ Applications/CSVFiles.py
@@ -1,5 +1,21 @@
 import csv
 
 print('CVS File Manipulation')
-with open('brasil_covid.csv', 'r') as cvsFile:
-    reader = csv.reader(cvsFile)
+with open('brasil_covid.csv', 'r') as csvFile:
+    reader = csv.reader(csvFile)
+    for line in reader:
+        print(line)
+
+with open('brasil_covid.csv', 'r') as csvFile:
+    reader = csv.reader(csvFile)
+    header = next(reader)
+    for line in reader:
+        if float(line[3])>1:
+            print(line)
+
+with open('brasil_covid.csv', 'r') as csvFile:
+    lines = csvFile.read()
+    lines = lines.split('\n')
+    for line in lines:
+        line = line.split(',')
+        print(line)
