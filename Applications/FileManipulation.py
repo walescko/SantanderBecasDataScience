abaca566f137d0889a40149fe65c2eef1610d585
Walescko <walescko@gmail.com>
2022-09-18 17:11:59 -0300

    py - open/close single file

--- Applications/FileManipulation.py
+++ Applications/FileManipulation.py
@@ -1,1 +1,13 @@
 print('Manipulação de Arquivos')
+file = open('domcasmurro.txt', 'r')
+text = file.read()
+print(text)
+file.close()
+
+file = open('domcasmurro.txt', 'r')
+line = file.readline()
+while line != '':
+    print(line, end='')
+    line = file.readline()
+
+file.close()
