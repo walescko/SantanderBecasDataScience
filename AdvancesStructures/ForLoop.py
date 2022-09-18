be91dc10e3018e40353785ecbece3fe11ab3d2da
Walescko <walescko@gmail.com>
2022-09-18 15:39:00 -0300

    py - for

--- AdvancesStructures/ForLoop.py
+++ AdvancesStructures/ForLoop.py
@@ -1,1 +1,30 @@
 print('For - loop')
+
+cityNames = ['Esteio', 'Porto Alegre', 'Daytona', 'Varsóvia']
+for name in cityNames:
+    print(name)
+
+print('\nTupla\n')
+cityNames02 = 'Esteio', 'Porto Alegre', 'Daytona', 'Varsóvia'
+for name in cityNames02:
+    print(name)
+
+city = {
+    'name' : 'Daytona',
+    'state': 'Florida',
+    'population': 120.25
+}
+
+for key in city:
+    print(f'{key}: {city[key]}')
+
+for i in range(len(cityNames)):
+    print(i)
+
+for i in range(len(cityNames)):
+    cityNames[i] = 'Talladega'
+print(cityNames)
+
+print(list(range(10)))
+print(list(range(2, 10)))
+print(list(range(2, 10, 2)))
