dd4ee1c83467726523c6640756c903d92f998c62
Walescko <walescko@gmail.com>
2022-09-18 17:24:25 -0300

    py - open/close single file

--- Applications/FileManipulation.py
+++ Applications/FileManipulation.py
@@ -1,13 +1,28 @@
 print('Manipulação de Arquivos')
-file = open('domcasmurro.txt', 'r')
-text = file.read()
-print(text)
-file.close()
+# file = open('domcasmurro.txt', 'r')
+# text = file.read()
+# print(text)
+# file.close()
+#
+# file = open('domcasmurro.txt', 'r')
+# line = file.readline()
+# while line != '':
+#     print(line, end='')
+#     line = file.readline()
+# file.close()
+#
+# file = open('domcasmurro.txt', 'r')
+# for line in file:
+#     print(line, end='')
+# file.close()
+#
+# with open('domcasmurro.txt', 'r') as file:
+#     text = file.read()
+#     print(text)
 
-file = open('domcasmurro.txt', 'r')
-line = file.readline()
-while line != '':
-    print(line, end='')
-    line = file.readline()
+with open('textFile', 'w') as file:
+    file.write('Primeira linha em um texto\n')
+    file.write('Segunda linha em um texto\n')
 
-file.close()
+with open('textFile', 'r') as file:
+    print(file.read(), end='')
